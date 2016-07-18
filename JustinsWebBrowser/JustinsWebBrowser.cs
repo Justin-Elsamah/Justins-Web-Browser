41d796927654ba3c159ca463319d01861b365b55
Justin_Elsamah <jelsamah@edu.uwaterloo.ca>
2016-07-18 10:26:14 -0400

    Main file added

---
+++ JustinsWebBrowser/JustinsWebBrowser.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class JustinsWebBrowser : Form
+    {
+        public JustinsWebBrowser()
+        {
+            InitializeComponent();
+        }
+        //This code is ran when File,Exit is pressed
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        //This code is ran when About is pressed
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This program was made by Justin");
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+        //When "Search" gets pressed display website specified
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SearchToPage();
+        }
+        //This function will perform all searching
+        private void SearchToPage()
+        {
+            toolStripStatusLabel1.Text = "Search has started";
+            button1.Enabled = false;
+            textBox1.Enabled = false;
+            webBrowser1.Navigate(textBox1.Text);
+        }
+        //Code will run on keypress
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)ConsoleKey.Enter)
+                {
+                SearchToPage();
+            }
+        }
+        //This code will run when search is completed 
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            button1.Enabled = true;
+            textBox1.Enabled = true;
+            toolStripStatusLabel1.Text = "Search Complete";
+        }
+
+        private void webBrowser1_ProgressChanged(object sender, WebBrowserProgressChangedEventArgs e)
+        {
+            if(e.CurrentProgress>0 && e.MaximumProgress > 0)
+            {
+                toolStripProgressBar1.ProgressBar.Value = (int)(e.CurrentProgress * 100 / e.MaximumProgress);
+            }          
+        }
+    }
+}
